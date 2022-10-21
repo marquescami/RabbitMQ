f52e9550aa4365c7a8d21bb43be39bcb384839cb
marquescami <camilamsc@hotmail.com>
2022-10-20 21:55:42 -0300

    Filas com RabbitMQ

---
+++ EstoquePreco/estoquepreco/src/main/java/com/microservico/estoquepreco/constantes/RabbitmqConstantes.java
@@ -0,0 +1,8 @@
+package com.microservico.estoquepreco.constantes;
+
+public class RabbitmqConstantes {
+
+    public static String FILA_ESTOQUE = "ESTOQUE";
+    public static String FILA_PRECO = "PRECO";
+
+}
